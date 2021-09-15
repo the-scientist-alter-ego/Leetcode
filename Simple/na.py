5d0b6a8d0d941d220ce5480ec0317bb84f4f7d5d
the-scientist-alter-ego <51250467+the-scientist-alter-ego@users.noreply.github.com>
2021-09-15 09:35:47 +0800

    Update na.py

--- Simple/na.py
+++ Simple/na.py
@@ -1,1 +1,15 @@
-
+# 20. Valid Parenthesis
+## deque(), double-linked list, O(1) pop() and append()           
+## List can do pop() append() too, but O(n)
+from collections import deque
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dict_brkt = {')':'(',']':'[','}':'{'}
+        stack = deque()
+        for i in s: # only 
+            if i in dict_brkt:
+                if not stack or stack.pop() != dict_brkt[i]:
+                    return False
+            else:
+                stack.append(i)
+        return not stack
